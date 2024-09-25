6167000a6cf4d2eec757b8094c211fd681767f53
Lucas Carvalho <lucascor_rtb@hotmail.com>
2024-09-25 15:59:43 -0300

    feat(feature/filter): create filter layout

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,17 @@
 import { ref } from "vue";
 import Filter from "./components/Filter.vue";
 import TheWelcome from "./components/TheWelcome.vue";
-
-const show = ref(false);
-
-function toggleShow() {
-  console.log("oi");
-  show.value = !show.value;
-}
 </script>
 
 <template>
   <header>
     <h1>Solicitações cirúrgicas</h1>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <button @click="toggleShow">Toggle</button>
-      <p>Opa</p>
-    </div> -->
-    <Filter />
+    <div>
+      <Filter />
+    </div>
   </header>
 
-  <main>
-    <!-- <TheWelcome /> -->
-  </main>
+  <main></main>
 </template>
 
 
@@ -37,17 +23,13 @@ header {
   flex-direction: column;
   align-content: center;
   justify-content: center;
-  height: max-content;
   padding: 2rem 0;
+  place-items: center;
 }
 
 header h1 {
   font-weight: 600;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  margin-bottom: 2rem;
 }
 
 @media (min-width: 1024px) {
--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -4,26 +4,62 @@ import { ref } from "vue";
 
 <template>
   <div class="filterContainer">
-    <p class="flexBox">Filtre por:</p>
-    <div class="flexBox">
-      <div class="flexBox">
+    <h3 class="flexBox" style="margin-bottom: 0.5rem; font-weight: 500;">Se quiser, filtre por:</h3>
+    <div class="flexBox" style="gap: 1rem;">
+      <div class="flexBox" style="gap: .5rem;">
         <p>Médico:</p>
         <input />
       </div>
-      <div class="flexBox">
+      <div class="flexBox" style="gap: .5rem;">
         <p>Paciente:</p>
         <input />
       </div>
+    </div>
+    <div class="flexBox" style="margin-top: 1rem;">
       <button>Pesquisar</button>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .filterContainer {
-    
+  padding: 1rem 3rem;
+  border-radius: 6px;
+  border: 2px solid white;
+  font-size: 16px;
 }
+ input {
+  border: 1px solid white;
+  border-radius: 6px;
+  color: white;
+  background: transparent;
+  padding: .3rem 1rem;
+ }
+
+ input:focus {
+  outline: none;
+  border: 2px solid white;
+  font-size: 14px;
+  font-weight: 500;
+ }
+
+ button {
+  padding: .5rem 1rem;
+  color: white;
+  background: transparent ;
+  border: 2px solid white;
+  font-weight: 600;
+  border-radius: 6px;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+button:hover {
+  transform: scale(1.05);
+  transition: .1s linear;
+  color: #03a5ed;
+  background: white;
+ }
 
 .flexBox {
   display: flex;
