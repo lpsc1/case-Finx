baa814a1495f79efd17331718190591d4dc5d055
Lucas Carvalho <lucascor_rtb@hotmail.com>
2024-09-25 14:27:58 -0300

    create base structure

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+import Filter from "./components/Filter.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+
+const show = ref(false);
+
+function toggleShow() {
+  console.log("oi");
+  show.value = !show.value;
+}
+</script>
+
+<template>
+  <header>
+    <h1>Solicitações cirúrgicas</h1>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <button @click="toggleShow">Toggle</button>
+      <p>Opa</p>
+    </div> -->
+    <Filter />
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  height: max-content;
+  padding: 2rem 0;
+}
+
+header h1 {
+  font-weight: 600;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/Filter.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="filterContainer">
+    <p class="flexBox">Filtre por:</p>
+    <div class="flexBox">
+      <div class="flexBox">
+        <p>Médico:</p>
+        <input />
+      </div>
+      <div class="flexBox">
+        <p>Paciente:</p>
+        <input />
+      </div>
+      <button>Pesquisar</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.filterContainer {
+    
+}
+
+.flexBox {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
